ee2d85563db6836e5b7b41922b9f62681ab91de0
Sonja Stefani <sonja.stefani@tum.de>
2022-01-31 12:39:18 +0100

    Added rescan button to bluetooth scan screen

--- app/src/main/java/com/example/mentalabexplore/ConnectBluetoothActivity.kt
+++ app/src/main/java/com/example/mentalabexplore/ConnectBluetoothActivity.kt
@@ -14,17 +14,44 @@ import androidx.core.content.ContextCompat
 class ConnectBluetoothActivity : AppCompatActivity() {
     lateinit var selectedDevice: String
     var activeButton: TextView? = null
+    lateinit var devices: LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_connect_bluetooth)
 
+        devices = findViewById<LinearLayout>(R.id.bluetooth_devices)
+
+        scan()
+    }
+
+    fun connect(view: View) {
+        // TODO do all of this in another thread / in the background
+        // Currently, everything is blocked during connection
+        if(Model.connectDevice(selectedDevice)) {
+            val intent = Intent(this, DisplayDataActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        else {
+            val t = Toast.makeText(this, "Could not connect to selected device", Toast.LENGTH_SHORT)
+            t.show()
+        }
+    }
+
+    fun rescan(view: View) {
+        devices.removeAllViews()
+        // Might be a good idea to add a tiny wait period here so the user doesn't think the app is frozen if nothing new is found
+        scan()
+    }
+
+    fun scan() {
         val deviceSet = Model.scanDevices(applicationContext)
-        val devices = findViewById<LinearLayout>(R.id.bluetooth_devices)
+
 
         for (device in deviceSet) {
             val deviceView = TextView(this)
-            deviceView.setText(device)
+            deviceView.text = device
             deviceView.setPadding(0, 30, 0, 30)
             deviceView.textAlignment = View.TEXT_ALIGNMENT_CENTER
             deviceView.background = getDrawable(R.drawable.rounded_corners_gray)
@@ -40,20 +67,6 @@ class ConnectBluetoothActivity : AppCompatActivity() {
             val space = Space(this)
             space.setMinimumHeight(40)
             devices.addView(space)
-        }
-    }
-
-    fun connect(view: View) {
-        // TODO do all of this in another thread / in the background
-        // Currently, everything is blocked during connection
-        if(Model.connectDevice(selectedDevice)) {
-            val intent = Intent(this, DisplayDataActivity::class.java)
-            startActivity(intent)
-            finish()
-        }
-        else {
-            val t = Toast.makeText(this, "Could not connect to selected device", Toast.LENGTH_SHORT)
-            t.show()
         }
     }
 }
